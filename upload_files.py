25718c9ab4c10d35e088800047a3a4396335a6b4
meharfaizan <87762660+meharfaizan@users.noreply.github.com>
2023-10-01 23:49:52 +0500

    Update upload_files.py

--- upload_files.py
+++ upload_files.py
@@ -15,96 +15,81 @@ router = Router()
 
 
 class States(group.Group):
-    state_waiting = group.M()
-    state_uploading = group.M()
-    state_naming = group.M()
+  state_waiting = group.M()
+  state_uploading = group.M()
+  state_naming = group.M()
 
 
 @router.use()
 async def only_pm(handler, event):
-    if event.is_private:
-        try:
-            return await handler(event)
-        except Exception as e:
-            print("Error happened", e)
-            await event.reply(f"An error happened please retry:\nerror code : {e}")
-            fsm = ctx.CageCtx.get()  # get UserCage of current user
-            await fsm.set_state(States.state_waiting)
-            await fsm.set_data({"files": []})
+  if event.is_private:
+    try:
+      return await handler(event)
+    except Exception as e:
+      print("Error happened", e)
+      await event.reply(f"An error happened please retry:\nerror code : {e}")
+      fsm = ctx.CageCtx.get() # get UserCage of current user
+      await fsm.set_state(States.state_waiting)
+      await fsm.set_data({"files": []})
 
 
 @router.message(text.commands("start", prefixes="/") & (State.exact(States.state_waiting) | State.entry))
 async def response(event):
-    await event.reply("Hi! Send me a lot of files to zip.\nDo /done to finally zip them and send them back,")
-    fsm = ctx.CageCtx.get()  # get UserCage of current user
+  await event.reply("Hi! Send me a lot of files to zip.\nDo /done to finally zip them and send them back,")
+  fsm = ctx.CageCtx.get() # get UserCage of current user
 
-    await fsm.set_state(States.state_uploading)
-    await fsm.set_data({"files": []})
+  await fsm.set_state(States.state_uploading)
+  await fsm.set_data({"files": []})
 
 
 @router.message(State.exact(States.state_waiting) | State.entry)
 async def response(event):
-    await event.reply("Send /start to start")
+  await event.reply("Send /start to start")
 
 
 @router.message(text.commands("done", prefixes="/") & State.exact(States.state_uploading))
 async def finished(event):
-    fsm = ctx.CageCtx.get()  # get UserCage of current user
-    await fsm.set_state(States.state_naming)
-    await event.reply("Please Choose a name for the ZIP file. (no extensions)")
+  fsm = ctx.CageCtx.get() # get UserCage of current user
+  await fsm.set_state(States.state_naming)
+  await event.reply("Please Choose a name for the ZIP file. (no extensions)")
 
 
 @router.message(State.exact(States.state_naming))
 async def naming(event):
-    fsm = ctx.CageCtx.get()  # get UserCage of current user
-    await fsm.set_state(States.state_waiting)
-    data = await fsm.get_data()
-    files = data['files']
+  fsm = ctx.CageCtx.get() # get UserCage of current user
+  await fsm.set_state(States.state_waiting)
+  data = await fsm.get_data()
+  files = data['files']
 
-    msg = await event.reply("Downloading...")
-    with tempfile.TemporaryDirectory() as tmp_dirname:
-        with ZipFile(f'{tmp_dirname}/{event.text}.zip', 'w') as zipObj2:
-            for file in files:
-                path = await event.client.download_media(file, file=tmp_dirname)
-                zipObj2.write(path, basename(path))
-        await msg.edit(f"Finished uploading {len(files)} files. Uploading zip file..")
-        await event.reply(file=f'{tmp_dirname}/{event.text}.zip')
-    await fsm.set_data({"files": []})
+  msg = await event.reply("Downloading...")
+  with tempfile.TemporaryDirectory() as tmp_dirname:
+    with ZipFile(f'{tmp_dirname}/{event.text}.zip', 'w') as zipObj2:
+      for file in files:
+        path = await event.client.download_media(file, file=tmp_dirname)
+        zipObj2.write(path, basename(path))
+    await msg.edit(f"Finished uploading {len(files)} files. Uploading zip file..")
+    await event.reply(file=f'{tmp_dirname}/{event.text}.zip')
+  await fsm.set_data({"files": []})
 
 
 @router.message(State.exact(States.state_uploading))
 async def uploading(event):
-    if event.file:
-        fsm = ctx.CageCtx.get()  # get UserCage of current user
-        data = await fsm.get_data()
-        files = data['files']
-        files.append(event.message.media)
-        await fsm.set_data(data)
-        await event.reply(f"Saved {len(files)} so far!")
+  if event.file:
+    fsm = ctx.CageCtx.get() # get UserCage of current user
+    data = await fsm.get_data()
+    files = data['files']
+    files.append(event.message.media)
+    await fsm.set_data(data)
+    await event.reply(f"Saved {len(files)} so far!")
 
-    else:
-        await event.reply("Please send a file or /done to finish")
+  else:
+    await event.reply("Please send a file or /done to finish")
 
 
 def default_conf_maker() -> RuntimeConfig:
-    i = int(os.environ.get("API_ID", 12345))
-    a = os.environ.get('API_HASH')
-    b = os.environ.get('BOT_TOKEN')
-    s = os.environ.get('SESSION_NAME')
-    return RuntimeConfig(
-        bot_token=b,
-        app_id=i,
-        app_hash=a,
-        session_dsn=s,
-    )
-
-
-async def main():
-    main_router = Router().include(router)
-    await run(main_router, DictStorage(), conf_maker=default_conf_maker)
-
-
-if __name__ == "__main__":
-    import asyncio
-
-    asyncio.run(main())
+  i = int(os.environ.get("API_ID", 12345))
+  a = os.environ.get('API_HASH')
+  b = os.environ.get('BOT_TOKEN')
+  s = os.environ.get('SESSION_NAME')
+  return RuntimeConfig(
+    bot_token
