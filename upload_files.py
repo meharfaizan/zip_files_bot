fcf371c22b1103a8952473ced25b2ff40e6e09b3
meharfaizan <87762660+meharfaizan@users.noreply.github.com>
2023-10-02 00:30:37 +0500

    Update upload_files.py

--- upload_files.py
+++ upload_files.py
@@ -53,8 +53,8 @@ def main():
   dp.add_handler(CommandHandler("start", start, filters=Filters.chat(OWNER_ID)))
   dp.add_handler(MessageHandler(Filters.document & Filters.chat(OWNER_ID), zip_file))
   
- updater.start_polling()
- updater.idle()
+  updater.start_polling()
+  updater.idle()
 
  if __name__ == '__main__':
   main()
